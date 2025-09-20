dc1ef25ce2403661bf7ec0efaebc4dc5f569db10
Shiwam-Git29 <shiwam808460@gmail.com>
2025-09-21 02:43:51 +0530

    Added the Greater Number Script

---
+++ Greater.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter First Number :"))
+num2 = int(input("Enter Second Number :"))
+
+if(num1 > num2):
+    print("Num1 is Greater")
+elif(num2 > num1):
+    print("Num2 is Greater")
+else:
+    print("Both are Equal")
